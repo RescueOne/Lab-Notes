488981bdad934dcc7e66d58814136d0285aff31d
agoddijn <alex.goddijn@gmail.com>
2015-05-14 18:01:34 -0700

    mend

---
+++ Code/frequencyProgram/frequencyProgram.ino
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////
+// TINAH Template Program - UBC Engineering Physics 253
+// (nakane, 2015 Jan 2)  - Updated for use in Arduino IDE (1.0.6)
+/////////////////////////////////////////////////
+
+
+#include <phys253.h>          
+#include <LiquidCrystal.h>    
+
+
+void setup()
+{  
+  #include <phys253setup.txt>
+  Serial.begin(9600) ;
+  
+}
+
+
+
+
+void loop()
+{
+  LCD.setCursor(0,0); LCD.print("Frequency");
+  int waveIn;
+  waveIn = digitalRead(0);
+  if (waveIn == HIGH) {
+     long time = micros();
+     while (waveIn == HIGH) {waveIn = digitalRead(0);}
+     LCD.setCursor(0,1);
+     LCD.print(time);
+  }
+}
+
