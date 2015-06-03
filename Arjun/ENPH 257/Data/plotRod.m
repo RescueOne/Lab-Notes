0be6689de5aabf10f0c3f30df0849c8d7feb762d
arjunvenkatesh <arjun.venn@gmail.com>
2015-06-03 15:25:57 -0700

    added plotting function

---
+++ Arjun/ENPH 257/Data/plotRod.m
@@ -0,0 +1,19 @@
+%function to easily plot heating data
+function plotRod( time, T1, T2, T3, T4, Tamb)
+	
+	%check to make
+	if( size(time, 2) > size(T1, 2) )
+		time = time(1:size(T1, 2));
+	end
+
+	figure
+	plot(time, T1, 'y', time, T2, 'r', time, T3, 'g', time, T4, 'b', time, Tamb, 'm')
+	xlabel('Time (seconds)', 'Fontsize', 12);
+	ylabel('Temperature (Celsius)', 'Fontsize', 12);
+	legendLabels = {'Temperature 1: 0mm (Cool End)', 'Temperature 2: 10mm', 'Temperature 3: 20mm', 'Temperature 4: 30mm (Hot End)', 'Ambient Temperature'};
+	legend(legendLabels , 'Location', 'NorthWest', 'Fontsize', 12);
+	grid on
+	grid minor
+	axis([0 inf 10 100]);
+	title('Temparature vs Time at Various Positions', 'Fontsize', 14);
+end
