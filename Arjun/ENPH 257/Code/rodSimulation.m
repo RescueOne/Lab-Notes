3882e720f9651890fd8f60dbcc627039997e3095
arjunvenkatesh <arjun.venn@gmail.com>
2015-06-08 18:20:41 -0700

    updated data

---
+++ Arjun/ENPH 257/Code/rodSimulation.m
@@ -0,0 +1,80 @@
+%Number of steps
+Nx = 60; %Number of steps in length
+Nt = 100000; %Number of steps in time
+t0 = 0; %Start time (s)
+tf = 2500; %End time (s)
+x0 = 0; %Start length (m)
+xf = 0.3; %End length (m)
+
+%Constants
+kc = 13.5; %Convection coefficient of horizontal Al rod (W/(m^2K))
+k = 180; %Constant of conductivity of Al (W/(mK))
+a = 0.011; %Radius of the rod (m)
+e = .21; %Emmisivity of sandblasted Al rod
+SBc = 5.67e-8; %Stefan-Boltzmann constant (W/(m^2K^4))
+Tamb = 298; %Ambient temperature (K)
+Cp = 860; %Specific heat capacity of Al (J/(K kg))
+rho = 2.7e3; %Density of Al (kg/m^3)
+
+%Calculated numbers
+dx = (xf - x0)/(Nx);
+dt = (tf - t0)/(Nt);
+dT_convec = @(Tx) (kc*2*(Tx-Tamb)*dt)/(Cp*rho*a); %Temperature change due to convection (K)
+dT_rad = @(Tx) (e*SBc*2*(Tx.^4-Tamb^4)*dt)/(Cp*rho*a); %Temperature change due to radiation (K)
+dT_convec_end = @(Tx) (kc*(Tx-Tamb)*dt)/(Cp*rho*dx);
+dT_rad_end = @(Tx) (e*SBc*(Tx.^4-Tamb^4)*dt)/(Cp*rho*dx);
+dT = @(P) (P * dt)/(Cp * pi*a^2*dx*rho); %Temperature change in chunk (K)
+
+%==============
+%= Simulation =
+%==============
+
+%Constants
+Pinl = 12; %Power into the left side of the rod (W)
+
+%Storage
+T = zeros(Nx, Nt); %Array o temp over time, indices are (x,t)
+
+%initial
+T(:,1) = ones(Nx,1)*(273+28); %Set all temperatures to Tamb
+
+for time = 1:Nt-1
+    %power in to rod
+    T(1,time+1)=T(1,time)+dT(Pinl);
+    
+    %temperature changes due to conduction
+    %along rod
+    T(2:Nx-1,time+1)=T(2:Nx-1,time)+(k/(Cp*rho))*((T(3:Nx,time)-2*T(2:Nx-1,time)+T(1:Nx-2,time))./dx^2)*dt;
+    T(1,time+1) = T(1,time+1)+((k/(Cp*rho))*(T(2,time)-T(1,time+1))./dx^2)*dt;
+    T(Nx,time+1) = T(Nx,time)-((k/(Cp*rho))*(T(Nx,time)-T(Nx-1,time))./dx^2)*dt;
+    
+    %temperature changes due to loss in convection and radiation
+    %along rod
+    T(1:Nx,time+1)=T(1:Nx,time+1) - dT_convec(T(1:Nx,time+1)) - dT_rad(T(1:Nx,time+1));
+    
+    %at cold and hot end of rod
+    T(Nx,time+1)=T(Nx,time+1) - dT_convec_end(T(Nx,time+1)) - dT_rad_end(T(Nx,time+1));
+end
+
+timeSim = linspace(0,tf,Nt);
+
+% for position = 1:Nx
+%     plot(timeSim,(T(position,:)-273));
+%     hold on;
+% end
+
+plot(timeSim,(T(1,:)-273));
+hold on
+plot(timeSim,(T(Nx,:)-273));
+% plot(timeSim,(T(floor(Nx/3),:)-273));
+% plot(timeSim,(T(floor(2*Nx/3),:)-273));
+plot(timeSim,(T(20,:)-273))
+plot(timeSim,(T(40,:)-273));
+
+xlabel('Time (Seconds)');
+ylabel('Temp (C)');
+
+timeDATA = timeDATA(1:size(T1, 2));
+plot(timeDATA, T0, 'c', timeDATA, T1, 'y', timeDATA, T2, 'g', timeDATA, T3, 'r', timeDATA, T4, 'm')
+
+hold off
--- Arjun/ENPH 257/Data/plotRod.m
+++ Arjun/ENPH 257/Data/plotRod.m
@@ -7,13 +7,12 @@ function plotRod( time, T1, T2, T3, T4, Tamb)
 	end
 
 	figure
-	plot(time, T1, 'y', time, T2, 'r', time, T3, 'g', time, T4, 'b', time, Tamb, 'm')
+	plot(time, T1, 'y', time, T2, 'r', time, T3, 'g', time, T4, 'c', time, Tamb, 'm')
 	xlabel('Time (seconds)', 'Fontsize', 12);
 	ylabel('Temperature (Celsius)', 'Fontsize', 12);
 	legendLabels = {'Temperature 1: 0mm (Cool End)', 'Temperature 2: 10mm', 'Temperature 3: 20mm', 'Temperature 4: 30mm (Hot End)', 'Ambient Temperature'};
 	legend(legendLabels , 'Location', 'NorthWest', 'Fontsize', 12);
 	grid on
-	grid minor
 	axis([0 inf 10 100]);
 	title('Temparature vs Time at Various Positions', 'Fontsize', 14);
 end
