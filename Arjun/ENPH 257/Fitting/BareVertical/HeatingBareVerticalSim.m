bd5873f068ed1031818caee117acb72894ba12c5
arjunvenkatesh <arjun.venn@gmail.com>
2015-06-28 20:18:22 -0700

    Version 2 of the fit for vertical bare rod

--- Arjun/ENPH 257/Fitting/BareVertical/HeatingBareVerticalSim.m
+++ Arjun/ENPH 257/Fitting/BareVertical/HeatingBareVerticalSim.m
@@ -1,11 +1,9 @@
-clc;
-
 %Constants for fit
 kc = 10.7; %Convection coefficient of horizontal Al rod (W/(m^2K))
 k = 145; %Constant of conductivity of Al (W/(mK))
 e = 0.21; %Emmisivity of sandblasted Al rod
-Cp = 950; %Specific heat capacity of Al (J/(K kg))
-loss = .27; %Fractional loss of power to environment
+Cp = 975; %Specific heat capacity of Al (J/(K kg))
+loss = .25; %Fractional loss of power to environment
 
 %Power
 V = 15.1; %V, voltage into power resistor
