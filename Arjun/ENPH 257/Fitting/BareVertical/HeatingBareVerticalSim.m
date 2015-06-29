62cf798300b92722096fb668bef6be6c10ad0af3
arjunvenkatesh <arjun.venn@gmail.com>
2015-06-28 17:09:23 -0700

    added fractional loss variable and input voltage/current

--- Arjun/ENPH 257/Fitting/BareVertical/HeatingBareVerticalSim.m
+++ Arjun/ENPH 257/Fitting/BareVertical/HeatingBareVerticalSim.m
@@ -1,9 +1,15 @@
 %Constants for fit
 kc = 7.2; %Convection coefficient of horizontal Al rod (W/(m^2K))
-k = 110; %Constant of conductivity of Al (W/(mK))
+k = 170; %Constant of conductivity of Al (W/(mK))
 e = 0.3; %Emmisivity of sandblasted Al rod
 Cp = 857; %Specific heat capacity of Al (J/(K kg))
-Pinl = 8.2; %Power into the left side of the rod (W)
+loss = .75; %Fractional loss of power to environment
+
+%Power
+V = 15.1; %V, voltage into power resistor
+I = 0.890; %A, current into power resistor
+Ppower = V*I; %W
+Pinl = Ppower*loss; %Power into the left side of the rod (W)
 
 %Number of steps
 Nx = 130; %Number of steps in length
